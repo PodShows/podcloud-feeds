21776451f942a476ef200575fac48c28408fd752
Pof Magicfingers <pof@podshows.fr>
2019-03-19 13:18:13 +0100

    fix: remove complexity in db connect method

--- src/connectors/connection.js
+++ src/connectors/connection.js
@@ -1,35 +1,29 @@
 import Mongoose from "mongoose"
 Mongoose.Promise = global.Promise
 
+const connect_opts = {
+  useMongoClient: true,
+  autoReconnect: true,
+  reconnectTries: Number.MAX_VALUE,
+  bufferMaxEntries: 0
+}
+
 const connect = (conn_str, opts) => {
   opts.tried++
 
   return new Promise((resolve, reject) => {
-    Mongoose.connect(conn_str, {
-      useMongoClient: true,
-      autoReconnect: true,
-      reconnectTries: Number.MAX_VALUE,
-      bufferMaxEntries: 0
-    }).then(
-      () => {
-        console.log("Connected to opts")
-        resolve()
-      },
-      err => {
-        const retry_in = Math.floor(opts.tried / 2 * opts.spaced)
-        console.error(
-          `Could not connect to MongoDB (try ${opts.tried}/${
-            opts.retries
-          }, will retry in ${retry_in} seconds)`,
-          err
-        )
-        if (opts.tried >= opts.retries) {
-          reject(err)
-        } else {
-          setTimeout(() => resolve(connect(conn_str, opts)), retry_in * 1000)
-        }
-      }
-    )
+    Mongoose.connect(conn_str, connect_opts).then(resolve, err => {
+      console.error(
+        `Failed to connect to MongoDB (${opts.tried}/${opts.retries})`,
+        err
+      )
+
+      if (opts.tried >= opts.retries) return reject(err)
+
+      const retry_in = Math.floor(opts.tried / 2 * opts.spaced)
+      console.error(`Will retry to connect in ${retry_in} seconds`)
+      setTimeout(() => resolve(connect(conn_str, opts)), retry_in * 1000)
+    })
   })
 }
 
