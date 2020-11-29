cb78ccb47bba228f1203479468377c0d02e5066e
Pof Magicfingers <pof@pof.pm>
2020-11-29 20:04:43 +0100

    feat: add podcloud_url to episodes

--- src/schema/types/resolvers/episode.js
+++ src/schema/types/resolvers/episode.js
@@ -40,6 +40,16 @@ const Episode = {
 
     return url
   },
+  podcloud_url(item, args, ctx) {
+    const podcast_podcloud_url = Podcast.platforms(item.feed, args, ctx)
+      .podcloud_url
+
+    return podcast_podcloud_url
+      ? `${podcast_podcloud_url}/episode/${item._slugs[
+          item._slugs.length - 1
+        ] || item._id}`
+      : null
+  },
   episode_type(item) {
     return /^(full|bonus|trailer)$/.test(item.episode_type)
       ? item.episode_type
